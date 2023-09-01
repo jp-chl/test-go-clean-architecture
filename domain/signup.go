24ce18a89c886c5458967dd9f77aa43ad197d5d9
jp-chl <vera.r.juan.pablo@gmail.com>
2023-08-31 22:44:51 -0400

    v2

--- domain/signup.go
+++ domain/signup.go
@@ -2,17 +2,19 @@ package domain
 
 import "context"
 
-type LoginRequest struct {
+type SignUpRequest struct {
+	Name     string `form:"name"`
 	Login    string `form:"login"`
 	Password string `form:"password"`
 }
 
-type LoginResponse struct {
+type SignUpResponse struct {
 	AccessToken  string `json:"accessToken"`
 	RefreshToken string `json:"refreshToken"`
 }
 
-type LoginUseCase interface {
+type SignUpUseCase interface {
+	Create(user *User) error
 	GetUserByLogin(c context.Context, login string) (User, error)
 	CreateAccessToken(user *User, secret string, expiry int) (accessToken string, err error)
 	CreateRefreshToken(user *User, secret string, expiry int) (refreshToken string, err error)
